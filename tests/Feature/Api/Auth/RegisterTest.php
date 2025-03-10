0e91729e299616ccb2d7125be6371a2062f0f466
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-03-10 07:17:21 +0000

    test: it should return the email has already been taken

--- tests/Feature/Api/Auth/RegisterTest.php
+++ tests/Feature/Api/Auth/RegisterTest.php
@@ -29,3 +29,30 @@ it('should be able to register a new user and retrieve the token', function () {
 
     $this->assertDatabaseCount($model->getTable(), 1);
 });
+
+it('should return the email has already been taken', function () {
+    $model = new User;
+
+    $existingUser = User::factory()->create();
+    $payload = [
+        'name' => fake()->name(),
+        'email' => $existingUser->email,
+        'password' => 'P@ssw0rd',
+        'password_confirmation' => 'P@ssw0rd',
+    ];
+
+    $response = $this->postJson(route('api.auth.register'), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJsonValidationErrors(['email']);
+
+    expect($response->json('errors.email.0'))->toBe('The email has already been taken.');
+
+    $this->assertDatabaseMissing($model->getTable(), [
+        'name' => $payload['name'],
+        'email' => $payload['email'],
+    ]);
+
+    $this->assertDatabaseCount($model->getTable(), 1);
+});
